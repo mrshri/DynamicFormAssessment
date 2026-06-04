12001cac67dbc97446d212047af90c3d56bb4c3a
Shri Nivas <mrshri404@gmail.com>
2026-06-04 20:02:54 +0530

    UI Implementation using MVC Razor pages

---
+++ DynamicForm.UI/Models/FormConfigViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DynamicForm.UI.Models
+{
+    public class FormConfigViewModel
+    {
+        public string FormKey { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public int Version { get; set; }
+
+        public List<FormFieldViewModel> Fields { get; set; } = new();
+    }
+}
